f91b6f802008621c03095712e1def3d0e37f2685
sr972 <sr972@cornell.edu>
2019-02-22 15:16:03 -0500

    Vector Addition Using DRAM Version #2

--- software/spmd/brg_vvadd/main.c
+++ software/spmd/brg_vvadd/main.c
@@ -2,11 +2,12 @@
 // main.c
 //========================================================================
 // Author: Shady Agwa, shady.agwa@cornell.edu
-// Date: 21 February 2019 
+// Date: 22 February 2019 
 // Based on Prof. Batten Code
-// The code add two vectors using 16 cores, Tile 0 intialize the two vectors,
-// then the 16 tiles exectute the addition in parallel (Vector_Size/16),
-// then Tile 0 verify the results and ends the execution!!
+// The code add two vectors using 16 cores and DRAM, Tile 0 intializes the two vectors,
+// then the 16 tiles will exectute the addition in parallel (Vector_Size/16), the last
+// tile will execute the remainder of the Vector.
+// Tile 0 verifies the results and ends the execution!!
 
 #include "bsg_manycore.h"
 #include "bsg_set_tile_x_y.h"
@@ -19,14 +20,16 @@
 // Global data
 //------------------------------------------------------------------------
 
-int size = 16;
+// Define Vectors in DRAM.
+int g_src0[256]  __attribute__ ((section (".dram")));
+int g_src1[256]  __attribute__ ((section (".dram")));
+int g_dest[256]  __attribute__ ((section (".dram")));
+// Sizes of Vectors and loads of tiles.
 const int g_size = 256;
-int g_src0[256];
-int g_src1[256];
-int g_dest[256];
-
-int g_go_flags[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-int g_done_flags[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int size = ( g_size / ( NUM_X_TILES * NUM_Y_TILES ) );
+// Control Signals.
+int* g_go_flag = 0;
+int* g_done_flag = 0;
 
 //------------------------------------------------------------------------
 // Vector Vecctor Add function
@@ -35,12 +38,13 @@ int g_done_flags[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 void vvadd( int* dest, int* src0, int* src1, int size)
 {
   for ( int i = 0; i < size; i++) {
-    *( dest + i ) = *( src0 + i ) + *( src1 + i );
+    dest[i] = src0[i] + src1[i];
   }	
   // Trace Execution Phase.
   bsg_printf("E");
   // Set the done flag.
-  g_done_flags[( ( bsg_x * TILE_DIM ) + bsg_y )] = 1;
+  int* done_xy = bsg_remote_ptr( bsg_x, bsg_y, &( g_done_flag ) );
+  *( done_xy ) = 1;
 }
 
 //------------------------------------------------------------------------
@@ -53,9 +57,12 @@ int main()
   bsg_set_tile_x_y();
   int num_tiles = bsg_num_tiles;
   int tile_id   = bsg_x_y_to_id( bsg_x, bsg_y );  
-
   // Determine where this tile should start in the data array.
-  int start_id = tile_id*num_tiles;
+  int start_id = tile_id * num_tiles;
+  // Last tile will handle the remainder.
+  if ( tile_id == ( num_tiles - 1 ) ) {
+    size = size + ( g_size % num_tiles );
+  }
 
   // Tile 0 will fill in the input data
   if ( tile_id == 0 ) {
@@ -63,31 +70,28 @@ int main()
       g_src0[i] = i;
       g_src1[i] = i * 10;    
     }
-    for ( int i = 1; i < num_tiles; i++ ) {
-      g_go_flags[i] = 1;
-    }
+    // write Tile 0 go flag for all tiles.
+    int* go = bsg_remote_ptr( 0, 0, &( g_go_flag ) );
+    *( go ) = 1;
   }
   else {
-    int * flag = bsg_remote_ptr( 0, 0, &( g_go_flags[tile_id] ) );
-    while ( !( *( flag ) ) ) {
-      // Trace the Waiting Phase.
-      bsg_printf("W"); 
+        int* go_t0 = bsg_remote_ptr( 0, 0, &( g_go_flag ) ) ;  
+        while ( !( *( go_t0 ) ) ) {
+          // Trace the Waiting Phase.
+          bsg_printf("W"); 
     }
   }
  
-  // Execute vvadd for just this tile's partition.
-  int * src0_pt = bsg_remote_ptr( 0, 0, &( g_src0[start_id] ) ) ;
-  int * src1_pt = bsg_remote_ptr( 0, 0, &( g_src1[start_id] ) ) ;
-  int * dest_pt = bsg_remote_ptr( 0, 0, &( g_dest[start_id] ) ) ;
-  vvadd( dest_pt, src0_pt, src1_pt, size); 
-     
+  // Execute vvadd for just this tile's partition. 
+  vvadd( &( g_dest[start_id] ), &( g_src0[start_id] ), &( g_src1[start_id] ), size );
+
   // Tile 0 will wait until all tiles are done.
   if ( tile_id == 0 ) {
     for ( int i = 0; i < NUM_X_TILES; i++ )
     for ( int j = 0; j < NUM_Y_TILES; j++ ) {
-      int * done  = bsg_remote_ptr( i, j, &( g_done_flags[(i*4)+j] ) ); 
+      int * done  = bsg_remote_ptr( i, j, &( g_done_flag ) ); 
       while ( !( *( done ) ) ) {
-        bsg_printf(".");
+        bsg_printf("w");
       }
     }
 
@@ -101,7 +105,7 @@ int main()
       }
     }
     if ( passed ) {
-      bsg_printf("\n\n *** _________# PASSED #_________ *** \n\n");
+      bsg_printf("\n\n #### _________# PASSED #_________ #### \n\n");
     }
     bsg_finish(); 
   }

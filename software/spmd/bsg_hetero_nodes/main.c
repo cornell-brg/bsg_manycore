2895ce0465f6893f9e349020cd69724d8b707601
shaolin <shawnless.xie@gmail.com>
2019-03-10 12:00:22 -0700

    [hetero_nodes] added the example code for heterogenous nodes

---
+++ software/spmd/bsg_hetero_nodes/main.c
@@ -0,0 +1,53 @@
+
+#include "bsg_manycore.h"
+#include "bsg_set_tile_x_y.h"
+
+/************************************************************************
+ Declear an array in DRAM. 
+*************************************************************************/
+int data[4] __attribute__ ((section (".dram"))) = { -1, 1, 0xF, 0x80000000};
+
+int main()
+{
+   int i;
+  /************************************************************************
+   This will setup the  X/Y coordination. Current pre-defined corrdinations 
+   includes:
+        __bsg_x         : The X cord inside the group 
+        __bsg_y         : The Y cord inside the group
+        __bsg_org_x     : The origin X cord of the group
+        __bsg_org_y     : The origin Y cord of the group
+  *************************************************************************/
+  bsg_set_tile_x_y();
+
+  /************************************************************************
+   Basic IO outputs bsg_remote_ptr_io_store(IO_X_INDEX, Address, Value)
+   Every core will outputs once.
+  *************************************************************************/
+  bsg_remote_ptr_io_store(IO_X_INDEX,0x1260,__bsg_x);
+
+  /************************************************************************
+   Example of Using Prinf. 
+   Please call printf once a time, other wise the output string will be 
+   messed up.  
+   A mutex in printf should release this constraint.
+  *************************************************************************/
+  if ((__bsg_x == bsg_tiles_X-1) && (__bsg_y == bsg_tiles_Y-1)) {
+
+     bsg_printf("\nManycore>> Hello from core %d, %d in group origin=(%d,%d).\n", \
+                        __bsg_x, __bsg_y, __bsg_grp_org_x, __bsg_grp_org_y);
+
+     bsg_printf("Manycore>> Values in DRAM:");
+     for(i=0; i<4; i++)
+        bsg_printf("%08x,",data[i]);
+     bsg_printf("\n\n");
+
+  /************************************************************************
+    Terminates the Simulation
+  *************************************************************************/
+    bsg_finish();
+  }
+
+  bsg_wait_while(1);
+}
+

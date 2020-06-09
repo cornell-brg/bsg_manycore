9aac08c262d8edb20521fb830f6c7769f91f1fc7
pp482 <pp482@cornell.edu>
2020-06-09 12:33:29 -0400

    Add line trace to CGRA xcel

--- v/brg_cgra_xcel/brg_cgra_xcel.v
+++ v/brg_cgra_xcel/brg_cgra_xcel.v
@@ -159,4 +159,131 @@ module brg_cgra_xcel
       .returned_yumi_o       ( returned_yumi_li      )
     );
 
+  // synopsys translate_off
+
+  string ce0, ce1, ce2, ce3;
+  string pe0, pe1, pe2, pe3;
+  string me0, me1, me2, me3;
+
+  function string get_ce_trace(logic mem_req_en, logic mem_resp_en, logic send_cfg_en);
+    if(mem_req_en)
+      get_ce_trace = "M ";
+    else if(mem_resp_en)
+      get_ce_trace = " >";
+    else if(send_cfg_en)
+      get_ce_trace = "C ";
+    else
+      get_ce_trace = "  ";
+  endfunction
+
+  function string get_pe_trace(logic [4:0] op, logic [31:0] a, logic [31:0] b, logic [31:0] out, logic operand_deq_en, logic result_send_en, logic recv_cfg_en, int num_ops);
+    string op_string, a_string, b_string, out_string;
+    if(recv_cfg_en)
+      get_pe_trace = ">C";
+    else begin
+      if(op == 0)
+        op_string = "->";
+      else if(op == 2)
+        op_string = "+ ";
+      else
+        op_string = "NA";
+      if(operand_deq_en & result_send_en)
+        get_pe_trace = op_string;
+      else
+        get_pe_trace = "  ";
+    end
+  endfunction
+
+  function string get_me_trace(logic mem_req_en, logic mem_resp_en, logic recv_cfg_en, int is_resp_dir_reversed);
+    if(recv_cfg_en) begin
+      get_me_trace = ">C";
+    end
+    else if(mem_req_en & mem_resp_en) begin
+      if(is_resp_dir_reversed)
+        get_me_trace = "M<";
+      else
+        get_me_trace = "M>";
+    end
+    else if(mem_req_en)
+      get_me_trace = "M ";
+    else if(mem_resp_en) begin
+      if(is_resp_dir_reversed)
+        get_me_trace = " <";
+      else
+        get_me_trace = " >";
+    end
+    else
+      get_me_trace = "  ";
+  endfunction
+
+  always_ff @ (negedge clk_i) begin
+    if(hb_cgra_xcel.cgra_xcel.ctrl.state == 1) begin
+      ce0 = get_ce_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_s.mem_master__req__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_s.mem_master__resp__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_s.cfg_send__en);
+      ce1 = get_ce_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_c__0.mem_master__req__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_c__0.mem_master__resp__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_c__0.cfg_send__en);
+      ce2 = get_ce_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_c__1.mem_master__req__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_c__1.mem_master__resp__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_c__1.cfg_send__en);
+      ce3 = get_ce_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_n.mem_master__req__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_n.mem_master__resp__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.CE_n.cfg_send__en);
+      me0 = get_me_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__0.mem_ifc__req__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__0.mem_ifc__resp__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__0.ctrl.self_cfg_deq,
+                         0);
+      me1 = get_me_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__1.mem_ifc__req__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__1.mem_ifc__resp__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__1.ctrl.self_cfg_deq,
+                         0);
+      me2 = get_me_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__2.mem_ifc__req__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__2.mem_ifc__resp__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__2.ctrl.self_cfg_deq,
+                         1);
+      me3 = get_me_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__3.mem_ifc__req__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__3.mem_ifc__resp__en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.ME_r__3.ctrl.self_cfg_deq,
+                         1);
+      pe0 = get_pe_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__0.ctrl.cfg.func,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__0.dpath.opd_a_mux.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__0.dpath.opd_b_mux.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__0.dpath.node.alu.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__0.ctrl.operands_deq_rdy & hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__0.ctrl.node_recv_rdy,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__0.ctrl.node_send_en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__0.ctrl.self_cfg_deq,
+                         1);
+      pe1 = get_pe_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__1.ctrl.cfg.func,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__1.dpath.opd_a_mux.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__1.dpath.opd_b_mux.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__1.dpath.node.alu.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__1.ctrl.operands_deq_rdy & hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__1.ctrl.node_recv_rdy,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__1.ctrl.node_send_en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__1.ctrl.self_cfg_deq,
+                         1);
+      pe2 = get_pe_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__2.ctrl.cfg.func,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__2.dpath.opd_a_mux.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__2.dpath.opd_b_mux.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__2.dpath.node.alu.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__2.ctrl.operands_deq_rdy & hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__2.ctrl.node_recv_rdy,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__2.ctrl.node_send_en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__2.ctrl.self_cfg_deq,
+                         1);
+      pe3 = get_pe_trace(hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__3.ctrl.cfg.func,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__3.dpath.opd_a_mux.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__3.dpath.opd_b_mux.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__3.dpath.node.alu.out,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__3.ctrl.operands_deq_rdy & hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__3.ctrl.node_recv_rdy,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__3.ctrl.node_send_en,
+                         hb_cgra_xcel.cgra_xcel.dpath.cgra.dpath.PE_rc__3.ctrl.self_cfg_deq,
+                         2);
+      // Only dump out trace when CGRA has launched
+      $display("[CGRA_XCEL] %s %s %s | %s %s %s | %s %s %s | %s %s %s",
+               ce0, me0, me1, ce1, pe0, pe1, ce2, pe2, pe3, ce3, me2, me3);
+    end
+  end
+
+  // synopsys translate_on
+
 endmodule

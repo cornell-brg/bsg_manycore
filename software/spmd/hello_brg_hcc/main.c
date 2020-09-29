84518a4cc5afcca38716f9e73c11c8d76f974cfb
Moyang Wang <mw828@cornell.edu>
2020-09-29 15:52:58 -0400

    add hello_world printf to hello_brg_hcc

--- software/spmd/hello_brg_hcc/main.c
+++ software/spmd/hello_brg_hcc/main.c
@@ -3,19 +3,17 @@
 int main()
 {
   brg_hcc_set_tile_x_y();
-  // int i = 0;
-  // char stderr_msg[] = "Hello world from BRG-HCC tile!\n";
-  // while(stderr_msg[i]) {
-  //   bsg_putchar_err(stderr_msg[i]);
-  //   cache_flush();
-  //   i++;
-  // }
 
-  /************************************************************************
-    Terminates the Simulation
-  *************************************************************************/
+  int i = 0;
+  char stderr_msg[] = "Hello world from BRG-HCC tile!\n";
+  while (stderr_msg[i]) {
+    bsg_putchar(stderr_msg[i]);
+    cache_flush(); // we need to flush the cache in order to send the char to a global addr
+    i++;
+  }
 
-  brg_hcc_finish(); // similar to bsg_finish() but with appropriate cache flush()
+  // Terminates the Simulation
+  brg_hcc_finish();
 
   // should not be reachable
   bsg_wait_while(1);

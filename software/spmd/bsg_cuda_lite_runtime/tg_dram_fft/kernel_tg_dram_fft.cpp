e77f10bed313d5ed9092cd73b35df7e4dbaa3daf
pp482 <pp482@cornell.edu>
2021-10-05 16:15:59 -0400

    [tg-dram] init implementation

---
+++ software/spmd/bsg_cuda_lite_runtime/tg_dram_fft/kernel_tg_dram_fft.cpp
@@ -0,0 +1,39 @@
+// This kernel performs radix of two DIT FFT
+
+#include "bsg_manycore.h"
+#include "bsg_set_tile_x_y.h"
+
+#include "common_fft/common_fft.hpp"
+
+FP32Complex fft_workset[NUM_POINTS];
+
+extern "C" __attribute__ ((noinline))
+int
+kernel_tg_dram_fft(FP32Complex *in, FP32Complex *out, int N, int block_size_x) {
+    bsg_cuda_print_stat_kernel_start();
+
+    /* debug_print_complex(input, NUM_POINTS, "Before bit reverse"); */
+    bsg_cuda_print_stat_start(1);
+    opt_data_transfer(fft_workset, in, NUM_POINTS);
+    bsg_cuda_print_stat_end(1);
+
+    bsg_cuda_print_stat_start(2);
+    opt_bit_reverse(fft_workset, NUM_POINTS);
+    bsg_cuda_print_stat_end(2);
+    /* debug_print_complex(fft_workset, NUM_POINTS, "After bit reverse"); */
+
+    bsg_cuda_print_stat_start(3);
+    fft_256(fft_workset);
+    bsg_cuda_print_stat_end(3);
+
+    bsg_cuda_print_stat_start(4);
+    opt_data_transfer(out, fft_workset, NUM_POINTS);
+    bsg_cuda_print_stat_end(4);
+
+    bsg_cuda_print_stat_kernel_end();
+    /* debug_print_complex(fft_workset, NUM_POINTS, "Results"); */
+
+    tg_barrier.sync();
+
+    return 0;
+}

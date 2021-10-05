e9e0ca6e08d248d8fa3ee8b6143672c593af117e
pp482 <pp482@cornell.edu>
2021-10-05 00:38:37 -0400

    [opt-single] FP opts

--- software/spmd/bsg_cuda_lite_runtime/opt_single_fft/kernel_opt_single_fft.cpp
+++ software/spmd/bsg_cuda_lite_runtime/opt_single_fft/kernel_opt_single_fft.cpp
@@ -26,6 +26,36 @@ FP32Complex fft_workset[NUM_POINTS];
 #define bsg_complex_store(x,y,local_addr,val) do { *(bsg_remote_complex((x),(y),(local_addr))) = (FP32Complex) (val); } while(0)
 #define bsg_complex_load(x,y,local_addr,val) do { val = *(bsg_remote_complex((x),(y),(local_addr))); } while(0)
 
+/* void */
+/* debug_print_complex(const FP32Complex *list, const int N, const char *msg) { */
+/*     bsg_printf("%s\n", msg); */
+/*     for(int i = 0; i < N; i++) { */
+/*         float rr = list[i].real(); */
+/*         float ii = list[i].imag(); */
+/*         bsg_printf("(%08X)+(%08X)i ", *(int*)&rr, *(int*)&ii); */
+/*     } */
+/* } */
+
+/* inline void */
+/* opt_data_transfer(FP32Complex *dst, const FP32Complex *src, const int N) { */
+/*     int i = 0; */
+/*     for (; i < N-4; i += 4) { */
+/*         register FP32Complex tmp0 = src[i    ]; */
+/*         register FP32Complex tmp1 = src[i + 1]; */
+/*         register FP32Complex tmp2 = src[i + 2]; */
+/*         register FP32Complex tmp3 = src[i + 3]; */
+/*         asm volatile("": : :"memory"); */
+/*         dst[i    ] = tmp0; */
+/*         dst[i + 1] = tmp1; */
+/*         dst[i + 2] = tmp2; */
+/*         dst[i + 3] = tmp3; */
+
+/*     } */
+/*     // fixup */
+/*     for (; i < N; i++) */
+/*         dst[i] = src[i]; */
+/* } */
+
 int workset_idx = 0;
 
 void
@@ -41,13 +71,32 @@ fft_swizzle(int start, int stride) {
     }
 }
 
+inline void
+opt_bit_reverse(FP32Complex *list, const int N) {
+    // Efficient bit reverse
+    // http://wwwa.pikara.ne.jp/okojisan/otfft-en/cooley-tukey.html
+    // The idea is to perform a reversed binary +1. The inner for loop
+    // executes M times, where M is the number of carries in the
+    // reversed binary +1 operation.
+    for (int i = 0, j = 1; j < N-1; j++) {
+        for (int k = N >> 1; k > (i ^= k); k >>= 1);
+        // after the for loop above i is bit-reversed j
+        if (i < j) {
+            register FP32Complex tmp = list[i];
+            list[i] = list[j];
+            list[j] = tmp;
+        }
+    }
+}
+
 void
 fft() {
     int even_idx, odd_idx, n = 2;
     float k_div_n;
     FP32Complex exp_val, tw_val, even_val, odd_val;
-    float PI = std::acos(-1);
-    FP32Complex ImagURoot = FP32Complex(0.0, 1.0);
+    const float PI = 3.14159265358979323846;
+    /* FP32Complex ImagURoot = FP32Complex(0.0, 1.0); */
+    FP32Complex exp_val_common = FP32Complex(0.0, -2.0f*PI);
 
     while (n <= NUM_POINTS) {
         for (int i = 0; i < NUM_POINTS; i += n) {
@@ -56,7 +105,7 @@ fft() {
                 odd_idx  = even_idx + n/2;
 
                 k_div_n = (float) k / (float) n;
-                exp_val = -2.0f*ImagURoot*PI*k_div_n;
+                exp_val = exp_val_common*k_div_n;
 
                 even_val = fft_workset[even_idx];
                 odd_val  = fft_workset[odd_idx];
@@ -70,6 +119,7 @@ fft() {
         n = n * 2;
     }
 
+    /* opt_data_transfer(output, fft_workset, NUM_POINTS); */
     for (int i = 0; i < NUM_POINTS; i++) {
         output[i] = fft_workset[i];
     }
@@ -77,40 +127,26 @@ fft() {
 
 extern "C" __attribute__ ((noinline))
 int
-kernel_unopt_single_fft(FP32Complex *in, FP32Complex *out, int N) {
+kernel_opt_single_fft(FP32Complex *in, FP32Complex *out, int N) {
     if (bsg_id == 0) {
         bsg_cuda_print_stat_kernel_start();
         input = in;
         output = out;
-        /* bsg_printf("Before swizzle:\n"); */
-        /* for(int i = 0; i < NUM_POINTS; i++) { */
-        /*     float rr = input[i].real(); */
-        /*     float ii = input[i].imag(); */
-        /*     bsg_printf("(%08X)+(%08X)i ", *(int*)&rr, *(int*)&ii); */
-        /* } */
 
+        /* debug_print_complex(input, NUM_POINTS, "Before bit reverse"); */
+        /* opt_data_transfer(fft_workset, input, NUM_POINTS); */
+        /* opt_bit_reverse(fft_workset, NUM_POINTS); */
         bsg_cuda_print_stat_start(1);
         fft_swizzle(0, 1);
         bsg_cuda_print_stat_end(1);
-
-        /* bsg_printf("\nAfter swizzle:\n"); */
-        /* for(int i = 0; i < NUM_POINTS; i++) { */
-        /*     float rr = fft_workset[i].real(); */
-        /*     float ii = fft_workset[i].imag(); */
-        /*     bsg_printf("(%08X)+(%08X)i ", *(int*)&rr, *(int*)&ii); */
-        /* } */
+        /* debug_print_complex(fft_workset, NUM_POINTS, "After bit reverse"); */
 
         bsg_cuda_print_stat_start(2);
         fft();
         bsg_cuda_print_stat_end(2);
 
         bsg_cuda_print_stat_kernel_end();
-        /* bsg_printf("\nResults:\n"); */
-        /* for(int i = 0; i < NUM_POINTS; i++) { */
-        /*     float rr = fft_workset[i].real(); */
-        /*     float ii = fft_workset[i].imag(); */
-        /*     bsg_printf("(%08X)+(%08X)i ", *(int*)&rr, *(int*)&ii); */
-        /* } */
+        /* debug_print_complex(fft_workset, NUM_POINTS, "Results"); */
     }
     return 0;
 }

1899885ece3ce29b813324bb189e4d3424f31520
lc873 <lc873@cornell.edu>
2022-03-03 15:31:44 -0500

    [appl] wip -- allocate space in DRAM for ref counts

--- software/spmd/bsg_cuda_lite_runtime/appl-fib/appl-config.h
+++ software/spmd/bsg_cuda_lite_runtime/appl-fib/appl-config.h
@@ -3,9 +3,12 @@
 
 #include <cstddef>
 
+#define MAX_WORKERS (bsg_tiles_X * bsg_tiles_Y)
+#define HB_L2_CACHE_LINE_WORDS 16
+
 // per-tile thread local variable to hold fast rand seed
 extern int seed
 // parallel for grain size
-extern size_t g_pfor_grain_size = 1;
+extern size_t g_pfor_grain_size;
 
 #endif

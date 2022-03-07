eaeb6802ea7ccf0598f66d74ea750e5c8233850c
lc873 <lc873@cornell.edu>
2022-03-07 11:21:02 -0500

    [appl-fib] make _pfor_grain_size uninited so it appears in sbss section

--- software/spmd/bsg_cuda_lite_runtime/appl-fib/appl-config.hpp
+++ software/spmd/bsg_cuda_lite_runtime/appl-fib/appl-config.hpp
@@ -13,7 +13,7 @@ extern int seed;
 extern size_t g_pfor_grain_size;
 
 // ref_count stack. This needs to be in the DRAM for AMO
-extern int ref_counts[MAX_WORKERS * HB_L2_CACHE_LINE_WORDS] __attribute__ ((section (".dram")));
 extern uint32_t ref_count_stack_idx;
+extern int ref_counts[MAX_WORKERS * HB_L2_CACHE_LINE_WORDS] __attribute__ ((section (".dram")));
 
 #endif

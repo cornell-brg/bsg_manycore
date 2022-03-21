afd7f453ed31bad147036aaf119c1323e86630e2
lc873 <lc873@cornell.edu>
2022-03-21 15:31:06 -0400

    [appl-lu] wip

--- software/spmd/appl/appl-parallel_invoke.hpp
+++ software/spmd/appl/appl-parallel_invoke.hpp
@@ -16,11 +16,13 @@ void parallel_invoke( const Func0& func0, const Func1& func1 );
 template <typename Func0, typename Func1, typename Func2>
 void parallel_invoke( const Func0& func0, const Func1& func1,
                       const Func2& func2 );
+*/
 
 template <typename Func0, typename Func1, typename Func2, typename Func3>
 void parallel_invoke( const Func0& func0, const Func1& func1,
                       const Func2& func2, const Func3& func3 );
 
+/*
 template <typename Func0, typename Func1, typename Func2, typename Func3,
           typename Func4,  typename Func5,  typename Func6>
 void parallel_invoke( const Func0& func0, const Func1& func1,

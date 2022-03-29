6f2d67ba9ae77b64a1de7844a8e9e55a04cceadb
lc873 <lc873@cornell.edu>
2022-03-29 13:30:09 -0400

    [vcore profiler] add amo dram and group

--- testbenches/common/v/vanilla_scoreboard_tracker_pkg.v
+++ testbenches/common/v/vanilla_scoreboard_tracker_pkg.v
@@ -5,6 +5,8 @@ package vanilla_scoreboard_tracker_pkg;
     logic remote_group_load;
     logic remote_global_load;
     logic remote_dram_load;
+    logic remote_dram_amo;
+    logic remote_group_amo;
     logic idiv;
   } vanilla_isb_info_s;
 

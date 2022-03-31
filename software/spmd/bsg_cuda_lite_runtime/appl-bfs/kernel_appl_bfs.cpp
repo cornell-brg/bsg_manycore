ea8034fb439f2ecd3cb003be03fe0dd57fe4a461
lc873 <lc873@cornell.edu>
2022-03-31 16:55:51 -0400

    [ligra-graph] memcpy graph to hb

--- software/spmd/bsg_cuda_lite_runtime/appl-bfs/kernel_appl_bfs.cpp
+++ software/spmd/bsg_cuda_lite_runtime/appl-bfs/kernel_appl_bfs.cpp
@@ -30,47 +30,20 @@ int32_t fib(int32_t n, int32_t gsize = 2) {
 }
 
 extern "C" __attribute__ ((noinline))
-int kernel_appl_bfs(int* results, int n, int grain_size, int* dram_buffer) {
+int kernel_appl_bfs(int* results, symmetricVertex* V, int n, int m, int* dram_buffer) {
 
   // debug print
   if (__bsg_id == 0) {
     bsg_print_int(n);
-    bsg_print_int(grain_size);
-    uintE neighbors[5];
-    neighbors[0] = 0;
-    neighbors[1] = 1;
-    neighbors[2] = 2;
-    neighbors[3] = 3;
-    neighbors[4] = 4;
-    symmetricVertex v = symmetricVertex(neighbors, 5);
-    graph<symmetricVertex> G = graph<symmetricVertex>(&v, 1, 5, nullptr);
-    bsg_print_int(G.V[0].getOutDegree());
-    bsg_print_int(G.V[0].getInNeighbor(2));
+    bsg_print_int(m);
+    graph<symmetricVertex> G = graph<symmetricVertex>(V, n, m, nullptr);
+    bsg_print_int(G.V[1].getOutDegree());
+    for (int i = 0; i < G.V[1].getOutDegree(); i++) {
+      bsg_print_int(G.V[1].getInNeighbor(i));
+    }
+    bsg_print_int(sizeof(symmetricVertex));
+    bsg_print_int(11111);
   }
-
-  // output
-  int32_t result     = -1;
-
-  // --------------------- kernel ------------------------
-  appl::runtime_init(dram_buffer, 2);
-
-  // sync
-  barrier.sync();
-  bsg_cuda_print_stat_kernel_start();
-
-  if (__bsg_id == 0) {
-    result = fib(n, grain_size);
-    results[0] = result;
-  } else {
-    appl::work_stealing_loop([&]() -> bool {
-        return bsg_amoadd(&appl::global::g_stop_flag, 0);
-        } );
-  }
-  appl::runtime_end();
-  // --------------------- end of kernel -----------------
-
-  bsg_cuda_print_stat_kernel_end();
-  bsg_print_int(result);
 
   barrier.sync();
   return 0;

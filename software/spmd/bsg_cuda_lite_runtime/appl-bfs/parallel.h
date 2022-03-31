3ad7ecf7ad10f3f3b36b17424975ba6004d6c96e
lc873 <lc873@cornell.edu>
2022-03-31 12:49:00 -0400

    [ligra] create a symmetric vertex

---
+++ software/spmd/bsg_cuda_lite_runtime/appl-bfs/parallel.h
@@ -0,0 +1,17 @@
+#ifndef _PARALLEL_H
+#define _PARALLEL_H
+
+#include <cstdint>
+#include <limits.h>
+
+typedef int32_t  intT;
+typedef uint32_t uintT;
+#define INT_T_MAX INT_MAX
+#define UINT_T_MAX UINT_MAX
+
+typedef int32_t  intE;
+typedef uint32_t uintE;
+#define INT_E_MAX INT_MAX
+#define UINT_E_MAX UINT_MAX
+
+#endif // _PARALLEL_H

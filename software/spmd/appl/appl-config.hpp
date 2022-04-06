07672b1a0f7d008a3bfad666c038c229d417121d
lc873 <lc873@cornell.edu>
2022-04-06 13:35:28 -0400

    [appl-api] Makefile hack -- if undefined, use applrts

--- software/spmd/appl/appl-config.hpp
+++ software/spmd/appl/appl-config.hpp
@@ -1,1 +1,6 @@
-#define APPL_IMPL_APPLRTS
+//========================================================================
+// appl-config.h
+//========================================================================
+
+// For debug, we can define APPL_IMPL_APPLRTS here
+// #define APPL_IMPL_APPLRTS

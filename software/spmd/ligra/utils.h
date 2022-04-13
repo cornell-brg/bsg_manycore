7bddf205926661464d725bd91889e7570936e2dc
lc873 <lc873@cornell.edu>
2022-04-13 13:59:24 -0400

    [ligra] writeAdd and kcore

--- software/spmd/ligra/utils.h
+++ software/spmd/ligra/utils.h
@@ -5,4 +5,12 @@
 
 #define newA( __E, __n ) (__E*)applrts::brg_malloc( ( __n ) * sizeof( __E ) )
 
+template <class ET>
+inline void writeAdd(ET* p, ET val) {
+  ET result;
+  asm volatile ("amoadd.w %[result], %[val], 0(%[p])" \
+                : [result] "=r" (result) \
+                : [p] "r" (p), [val] "r" (val));
+}
+
 #endif

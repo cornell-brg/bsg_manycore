d1956418ece6a3138b2465260a84914bf40c6fb4
lc873 <lc873@cornell.edu>
2022-04-18 12:04:50 -0400

    [ligra] writeMin/Max

--- software/spmd/ligra/utils.h
+++ software/spmd/ligra/utils.h
@@ -13,4 +13,36 @@ inline void writeAdd(ET* p, ET val) {
                 : [p] "r" (p), [val] "r" (val));
 }
 
+template <class ET>
+inline void writeMin(ET* p, ET val) {
+  ET result;
+  asm volatile ("amomin.w %[result], %[val], 0(%[p])" \
+                : [result] "=r" (result) \
+                : [p] "r" (p), [val] "r" (val));
+}
+
+template <class ET>
+inline void writeMinu(ET* p, ET val) {
+  ET result;
+  asm volatile ("amominu.w %[result], %[val], 0(%[p])" \
+                : [result] "=r" (result) \
+                : [p] "r" (p), [val] "r" (val));
+}
+
+template <class ET>
+inline void writeMax(ET* p, ET val) {
+  ET result;
+  asm volatile ("amomax.w %[result], %[val], 0(%[p])" \
+                : [result] "=r" (result) \
+                : [p] "r" (p), [val] "r" (val));
+}
+
+template <class ET>
+inline void writeMaxu(ET* p, ET val) {
+  ET result;
+  asm volatile ("amomaxu.w %[result], %[val], 0(%[p])" \
+                : [result] "=r" (result) \
+                : [p] "r" (p), [val] "r" (val));
+}
+
 #endif

e33f600c6a73ec727643423ee993120b8b96b92b
lc873 <lc873@cornell.edu>
2022-06-07 10:32:18 -0400

    [appl-malloc] allocate more space for tile0

--- software/spmd/appl/appl-malloc.cpp
+++ software/spmd/appl/appl-malloc.cpp
@@ -5,12 +5,22 @@ namespace appl {
 namespace local {
 
 uint32_t dram_buffer_idx = 0;
+uint32_t dram_buffer_end = 0;
 int* dram_buffer;
 
 } // namespace local
 
 void malloc_init(int* dram_buffer) {
-  local::dram_buffer = &(dram_buffer[__bsg_id * BUF_FACTOR * HB_L2_CACHE_LINE_WORDS]);
+  uint32_t total_cachelines = 128 * BUF_FACTOR;
+  uint32_t tile0_cachelines = total_cachelines - 128 * RT_FACTOR;
+
+  local::dram_buffer = &(dram_buffer[__bsg_id * RT_FACTOR * HB_L2_CACHE_LINE_WORDS]);
+  local::dram_buffer_end = RT_FACTOR * HB_L2_CACHE_LINE_WORDS;
+
+  if (__bsg_id == 0) {
+    local::dram_buffer = &(dram_buffer[128 * RT_FACTOR * HB_L2_CACHE_LINE_WORDS]);
+    local::dram_buffer_end = tile0_cachelines * HB_L2_CACHE_LINE_WORDS;
+  }
 }
 
 } // namespace appl
